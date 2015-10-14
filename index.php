b4e5ec1a28bbdcbdc8c8bb72af30d63d3e91abf6
metalx1000 <github@filmsbykris.com>
2015-10-14 16:28:43 -0400

    added people and favicon

--- index.php
+++ index.php
@@ -7,6 +7,29 @@
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<!--favicon-->
+<link rel="apple-touch-icon" sizes="57x57" href="icons/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="icons/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="icons/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="icons/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="icons/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="icons/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="icons/apple-touch-icon-144x144.png">
+<link rel="icon" type="image/png" href="icons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="icons/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="icons/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="icons/manifest.json">
+<link rel="mask-icon" href="icons/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="icons/favicon.ico">
+<meta name="apple-mobile-web-app-title" content="Toy Phone Dialer">
+<meta name="application-name" content="Toy Phone Dialer">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="icons/mstile-144x144.png">
+<meta name="msapplication-config" content="icons/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
+<!--end favicon-->
+
   <style>
     .main{
       height:100vh;
@@ -19,22 +42,43 @@
     }
   </style>
   <script>
+    var people = [ "Dada.1.ogg" ]
+    var dial = document.createElement('audio');
+    dial.setAttribute('src', 'sounds/dial1.ogg');
+    dial.setAttribute('autoplay', 'autoplay');
+    dial.play();
+    dial.pause();
+
     $(document).ready(function(){
       $(".num").click(function(){
-        play("dial1.wav");
+        dial.play();
         var num = $(this).html();
         $("#display").append(num);
+
+        //7 digites dial
+        var displayLen = $("#display").html().length;
+        if(displayLen == 8){
+          call();
+        }
       });
     });
 
-    function play(snd){
-        var audioElement = document.createElement('audio');
-        audioElement.setAttribute('src', 'sounds/'+snd);
-        audioElement.setAttribute('autoplay', 'autoplay');
-        
-        audioElement.addEventListener("load", function() {
-            audioElement.play();
-        }, true);
+    function call(){
+      var person = people[0];
+      var snd = document.createElement('audio');
+      snd.setAttribute('src', 'people/' + person);
+      snd.setAttribute('autoplay', 'autoplay');
+      snd.play();      
+
+      var name = person.split(".");
+      $("#display").html("Calling " + name[0]);
+      setTimeout(function(){
+        $("#display").html(name[0]);
+      },1000);
+
+      setTimeout(function(){
+        $("#display").html("");
+      },5000);
     }
   </script>
 </head>
