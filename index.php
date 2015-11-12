d2b6581b273604860838919bc1bb97f105070289
metalx1000 <github@filmsbykris.com>
2015-11-12 10:14:09 -0500

    added local css files

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <title>Toy Phone</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="lib/howler.min.js"></script>
